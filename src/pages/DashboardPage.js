f942ab8680f640d22a8cc9f502cfd734b951ba51
TeeJayGTAN <oniretemisan@gmail.com>
2022-07-22 16:28:48 -0700

    dasboard almost done

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -6,8 +6,18 @@ function DashboardPage() {
       <div className="navbar sticky top-0 bg-glass flex items-center justify-between px-4 py-3 md:px-6 md:py-4">
         <div className="flex items-center gap-2"><div className="logo w-[64px] md:w-[72px]"><img className='w-full' src="../logo.png" alt="" /> </div> <span className='sp text-xl md:text-3xl font-bold'>Survey</span></div>
         <div className="info flex gap-3 md:gap-4 items-center">
-          <div className="p-icon w-[32px] md:w-[48px]"><img className='w-full cursor-pointer' src="../user.png" alt="" /></div> <div className='text-lg'>Hi, <span>John Doe</span> </div> <div className='w-[20px] md:w-[24px] cursor-pointer'><img className='w-full' src="../menudown.png" alt="" /></div>
+          <div className="p-icon w-[24px] md:w-[32px]"><img className='w-full cursor-pointer' src="../user.png" alt="" /></div> <div className='text-sm'>Hi, <span className='name'>John Doe</span> </div> <div className='w-[20px] md:w-[24px] cursor-pointer'><img className='w-full' src="../menudown.png" alt="" /></div>
         </div>
+      </div>
+      <div className="banner mt-16 w-full mx-auto flex flex-col justify-center items-center gap-2 md:gap-4">
+        <div className="heading-info text-2xl md:text-3xl">Welcome <span className='name font-bold'>John Doe</span></div>
+        <div className="sub-inf text-sm md:text-base font-light">To get started, you can take a <span className='font-medium'>survey</span></div>
+        <div ><button className="text-sm mt-3 rounded bg-teal-600 py-3 px-5 text-white">TAKE A SURVEY</button></div>
+      </div>
+      <div className="actions grid grid-cols-1 md:grid-cols-3 px-4 md:px-6 gap-5 md:gap-6 md:mt-12 mt-9 text-sm text-white">
+        <div className="action bg-teal-600 rounded-full flex items-center justify-center p-8 cursor-pointer">Take survey</div>
+        <div className="action bg-teal-600 rounded-full flex items-center justify-center p-8 cursor-pointer">View result</div>
+        <div className="action bg-teal-600 rounded-full flex items-center justify-center p-8 cursor-pointer">View Learning Material</div>
       </div>
     </div>
   )
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -7,12 +7,12 @@ function SignupPage() {
       <div className="Signup-pics h-screen w-screen text-[white] ">
         {/* <img className='w-full h-full object-contain' src="../loginpics.jpg" alt="" /> */}
         <div className="login-img w-full h-full flex justify-center items-center ">
-          <div className="signup-form md:max-w-lg w-[90%] h-5/6 bg-glass py-6 px-4 flex flex-col justify-between items-center shadow">
+          <div className="signup-form md:max-w-lg w-[90%] gap-7 md:h-5/6 bg-glass py-6 px-4 flex flex-col justify-between items-center shadow">
             <div className="logo w-[128px] drop-shadow-2xl"><img src="../logo.png" className='w-full h-full' alt="" /></div>
             <div className="info mb-2 text-sm opacity-80"><span>Already have an account? <Link to='/'><span className='underline cursor-pointer'>Login</span></Link></span></div>
-            <div className="input-group flex items-center w-full justify-between md:text-lg px-9"><label htmlFor="username">User Name</label><input type="text" className='w-[70%] text-base rounded bg-glass px-3 py-3 text-[#000]'/></div>
-            <div className="input-group flex items-center w-full justify-between md:text-lg px-9"><label htmlFor="email">Email</label><input type="email" className='w-[70%] text-base rounded bg-glass px-3 py-3 text-[#000]'/></div>
-            <div className="input-group flex items-center w-full justify-between md:text-lg px-9"><label htmlFor="password">Password</label><input type="password" className='w-[70%] text-base rounded bg-glass px-3 py-2 text-[#000]'/></div>
+            <div className="input-group flex items-center w-full justify-between md:text-lg md:px-9"><label htmlFor="username">User Name</label><input type="text" className='w-[70%] text-base rounded bg-glass px-3 py-3 text-[#000]'/></div>
+            <div className="input-group flex items-center w-full justify-between md:text-lg md:px-9"><label htmlFor="email">Email</label><input type="email" className='w-[70%] text-base rounded bg-glass px-3 py-3 text-[#000]'/></div>
+            <div className="input-group flex items-center w-full justify-between md:text-lg md:px-9"><label htmlFor="password">Password</label><input type="password" className='w-[70%] text-base rounded bg-glass px-3 py-2 text-[#000]'/></div>
             <div className="signupbtn w-full "><button className='bg-glass w-full  rounded border-none text-lg py-3 px-5 mt-5'>Sign Up</button></div>
           </div>
         </div>
