21ecea1fa78be507bf1fb6816cab4bb8f42dcb39
TeeJayGTAN <oniretemisan@gmail.com>
2022-07-22 14:02:51 -0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="">
+    
+        <Routes>
+          <Route path ="/" element={<LoginPage/>}/>
+          <Route path ="/signup" element={<SignupPage/>}/>
+          <Route path ="/dashboard" element={<DashboardPage/>}/>
+
+        </Routes>
     </div>
+
+   
   );
 }
 
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+function LoginPage() {
+  return (
+    <div>
+      <div className="Signup-pics h-screen w-screen text-[white] ">
+        {/* <img className='w-full h-full object-contain' src="../loginpics.jpg" alt="" /> */}
+        <div className="login-img w-full h-full flex justify-center items-center ">
+          <div className="signup-form md:max-w-lg w-[90%] h-5/6 bg-glass py-6 px-4 flex flex-col justify-around items-center shadow">
+            <div className="logo w-[128px] drop-shadow-2xl"><img src="../logo.png" className='w-full h-full' alt="" /></div>
+            <div className="info mb-2 text-sm opacity-80"><span>Don't have an account yet? <Link to='/signup'><span className='underline cursor-pointer'>Signup</span></Link></span></div>
+            <div className="input-group flex items-center w-full justify-around md:text-lg px-9"><label htmlFor="username">User Name</label><input type="text" className='w-[70%] text-base rounded bg-glass px-3 py-3 text-[#000]'/></div>
+            <div className="input-group flex items-center w-full justify-around md:text-lg px-9"><label htmlFor="password">Password</label><input type="password" className='w-[70%] text-base rounded bg-glass px-3 py-3 text-[#000]'/></div>
+            <div className="signupbtn w-full"><button className='bg-glass w-full  rounded border-none text-lg py-3 px-5 mt-5'>Login</button></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
