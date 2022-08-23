083c23c4f983795df5d42eac2f82b41d1e45ef1f
TeeJayGTAN <oniretemisan@gmail.com>
2022-08-23 08:09:11 +0100

    post question not working

--- src/components/CreateQuestions.js
+++ src/components/CreateQuestions.js
@@ -13,12 +13,14 @@ function CreateQuestions() {
     setLoading(true);
     e.preventDefault();
     let token = localStorage.getItem("token");
+    // const myJSON = JSON.stringify(data)
     try {
       const res = await axios.post(
         "/admin/survey/post_question",
         {
           question,
-          adminID: user.id,
+      // options:["yes", "bye", "ok"],
+          admnID: user.id
         },
         {
           headers: {
@@ -26,7 +28,16 @@ function CreateQuestions() {
           },
         }
       );
+      // const res = await axios.get(
+      //   "/admin/survey",
+      //   {
+      //       headers: {
+      //            Authorization: "Bearer " + token,
+      //          },
+      //   },
+      // )
       console.log(res);
+      console.log(res.data.results)
       setMessage(res.data.message);
       setQuestion("");
       setLoading(false);
@@ -36,10 +47,12 @@ function CreateQuestions() {
       console.error(error);
     }
   };
+
+
   return (
     <form
       onSubmit={handleSubmit}
-      className="cardd bg-white w-[80%] md:max-w-lg h-[45%] shadow-2xl flex flex-col justify-center px-6 py-4 rounded md:rounded-lg"
+      className="cardd bg-white w-[80%] md:max-w-lg h-[83vh] shadow-2xl flex flex-col justify-center px-6 py-4 rounded md:rounded-lg"
     >
       <div className="mb-4 text-base font-bold md:text-lg">
         <p>Question</p>
@@ -48,15 +61,52 @@ function CreateQuestions() {
           <p className="text-xs font-medium text-red-500 capitalize">{error}</p>
         )}
       </div>
-      <div className="flex items-center justify-center w-full cursor-pointer ">
+      <div className="flex flex-col items-start justify-center w-full cursor-pointer ">
         <input
           type="text"
           className="w-full px-3 py-4 border-2 border-gray-500 rounded"
-          placeholder="Create Survey Question"
-          onChange={(e) => {
+          placeholder="Enter Survey Question Here"
+           onChange={(e) => {
             setQuestion(e.target.value);
-          }}
-          value={question}
+            console.log(question)
+           }}
+           value={question}
+        />
+        <input
+          type="text"
+          className="w-[60%] mt-8 text-sm px-2 py-3 border-2 border-gray-500 rounded"
+          placeholder="Option 1"
+          // onChange={(e) => {
+          //   setOption1(e.target.value);
+          // }}
+          // value={question}
+        />
+        <input
+          type="text"
+          className="w-[60%] mt-5 text-sm px-2 py-3 border-2 border-gray-500 rounded"
+          placeholder="Option 2"
+          // onChange={(e) => {
+          //   setOption2(e.target.value);
+          // }}
+          // value={question}
+        />
+        <input
+          type="text"
+          className="w-[60%] mt-5 text-sm px-2 py-3 border-2 border-gray-500 rounded"
+          placeholder="Option 3"
+          // onChange={(e) => {
+          //   setOption3(e.target.value);
+          // }}
+          // value={question}
+        />
+        <input
+          type="text"
+          className="w-[60%] mt-5 text-sm px-2 py-3 border-2 border-gray-500 rounded"
+          placeholder="Option 4"
+          // onChange={(e) => {
+          //   setOption4(e.target.value);
+          // }}
+          // value={question}
         />
       </div>
 
